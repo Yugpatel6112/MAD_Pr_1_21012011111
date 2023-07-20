922e74ef1890d9ecc1fd827d2233b493e6c8cc91
Yugpatel6112 <98756692+Yugpatel6112@users.noreply.github.com>
2023-07-20 22:34:01 +0530

    Practical-1.5.kt

---
+++ src/main/kotlin/Practical-1.5.kt
@@ -0,0 +1,21 @@
+fun main()
+{
+    print("Enter Month number:")
+    var a= readLine()!!.toInt()
+    when(a)
+    {
+        1-> println("January")
+        2-> println("February")
+        3-> println("March")
+        4-> println("April")
+        5-> println("May")
+        6-> println("June")
+        7-> println("July")
+        8-> println("August")
+        9-> println("Septmber")
+        10-> println("Octomber")
+        11-> println("November")
+        12-> println("December")
+        13-> println("Enter Valid Number")
+    }
+}
