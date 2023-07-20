0c5d80f207b3c23bf46e6129af315429330975e0
Yugpatel6112 <98756692+Yugpatel6112@users.noreply.github.com>
2023-07-20 21:58:32 +0530

    Practical-1.1.kt

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,34 @@
+fun main(){
+    println("hello world")
+
+
+    var a:Int=7
+    println("$a:integer variable")
+
+    var b:Float=1.43f
+    println("$b:float variable")
+
+    var c:String="kotlin"
+    println("$c:string variable")
+
+    var d:Double=1.234
+    println("$d: Double variable")
+
+    var e:Boolean=true
+    println("$e: Boolean variable")
+
+    var f:Char='a'
+    println("$f: Character variable")
+
+    var l:Long=21012011043
+    println("$l: long value")
+
+    var s:Short=43
+    println("$s: short variable")
+
+    var g:Byte=127
+    println("$g:Byte variable")
+
+    val x=3.14
+    println("$x: constant value")
+}
