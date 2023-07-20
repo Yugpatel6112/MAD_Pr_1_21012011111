856eaf23dc4da17d61cc34ad20f03255c105f733
Yugpatel6112 <98756692+Yugpatel6112@users.noreply.github.com>
2023-07-20 22:04:17 +0530

    Practical-1.3.kt

---
+++ src/main/kotlin/Practical-1.3.kt
@@ -0,0 +1,35 @@
+fun main()
+{
+    println("Enter Student information:")
+    println("Student Enrollment NO:")
+    var a= readLine()
+
+    println("Student Name:")
+    var b= readLine()
+
+    println("Student Branch:")
+    var c= readLine()
+
+    println("Student class:")
+    var d= readLine()
+
+    println("Student Batch:")
+    var e= readLine()
+
+    println("College Name:")
+    var f= readLine()
+
+    println("University Name:")
+    var g= readLine()
+
+
+    println("Student Data")
+    println("Student Enrollment NO: $a")
+    println("Student Name:$b")
+    println("Student Branch:$c")
+    println("Student class:$d")
+    println("Student Batch:$e")
+    println("College Name:$f")
+    println("University Name:$g")
+
+}
